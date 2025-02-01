7a843fe318bd951b7d877b3412c6ae0e31b1f261
hantaehee94 <hantaeh94@gail.com>
2025-02-01 23:00:45 +0900

    Hello from vscode

---
+++ Explorer:VS Code/review_list.py
@@ -0,0 +1,7 @@
+reviewlist = ["The Bodyguard", 7.0, 1992,[1,2],("A",1)]
+print('the same element using negative and positive indexing:\n Postive[0]:',reviewlist[0],
+'\n Negative[-5]:' , reviewlist[-5]  )
+print('the same element using negative and positive indexing:\n Postive[1]:',reviewlist[1],
+'\n Negative[-4]:' , reviewlist[-4]  )
+print('the same element using negative and positive indexing:\n Postive:[2]',reviewlist[2],
+'\n Negative[-3]:' , reviewlist[-3]  )
