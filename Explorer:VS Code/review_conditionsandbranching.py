640be540f4246bcdc0572846d5f8ba4c424ec486
hantaehee94 <hantaeh94@gail.com>
2025-02-04 00:38:32 +0900

    ddd

---
+++ Explorer:VS Code/review_conditionsandbranching.py
@@ -0,0 +1,35 @@
+# comparision operations
+a = 6
+a==5
+
+b = 5
+b > 3
+
+c = 8
+c >= 5
+
+d = 8
+d != 5
+
+"ACDC" == "Body Guard"
+"ACDC" != "Body Guard"
+
+# Branching
+age = 20
+if age > 18:
+    print("your age is above 18")
+else:
+    print("your age is below 18")
+print("Move on")
+
+# branching with elif
+age = 30
+if age > 18:
+    print("18살 이상임")
+elif age == 18:
+    print("18살임")
+else:
+    print("18살 미만임")
+print("Move on")
+
+# Logical operators
