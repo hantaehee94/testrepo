23f94731c5a5cabb3245dd58cb5a11a7de3afc5d
hantaehee94 <hantaeh94@gail.com>
2025-02-11 15:01:53 +0900

    why???

---
+++ Explorer:VS Code(python)/review_function.py
@@ -0,0 +1,17 @@
+def add(a):
+    """
+    add 1 to a
+    """
+    b = a + 1
+    print(a, "if you add one", b)
+    return(b)
+
+add(5)
+
+def mult(a,b):
+    c = a * b
+    print(c)
+    return(c)
+
+mult(6,7)
+
